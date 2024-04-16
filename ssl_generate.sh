dec0dce3e5a676de6f339afbdc633335d155ee13
arpaflow <145016532+arpaflow@users.noreply.github.com>
2024-04-16 11:44:26 +0200

    Update .gitignore and docker-compose.yaml

---
+++ ssl_generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define where we want to output our private key and certificate
+SSL_DIR="app/nginx/certs/"
+PRIVATE_KEY="${SSL_DIR}/selfsigned.key"
+CERTIFICATE="${SSL_DIR}/selfsigned.crt"
+
+# Generate a new private key if one doesn't already exist
+if [ ! -f "$PRIVATE_KEY" ]; then
+    openssl genrsa -out "$PRIVATE_KEY" 2048
+fi
+
+# Generate a new self-signed certificate
+openssl req -new -x509 -key "$PRIVATE_KEY" -out "$CERTIFICATE" -days 365
+
+# Print the location of the new certificate
+echo "The new certificate is located at $CERTIFICATE"
